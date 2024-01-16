4c5163887d2075641ae7f18d9949e39a805a4241
markjamesthuet <markjamesthuet@MARKJAMESTH936B>
2024-01-15 17:52:04 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using MoreCSharpFun;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PrintStuff ps = new PrintStuff();
+
+        string name = "";
+
+
+        System.Console.WriteLine("Please enter your name");
+        name = System.Console.ReadLine();
+
+        ps.PrintName();
+    }
+
+}
